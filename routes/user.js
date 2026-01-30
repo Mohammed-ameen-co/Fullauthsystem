c4f4d5bc2bba827fe83bf5664d5e1bb855f93e3a
Mohammed-ameen-co <aameenrangrej4@gmail.com>
2026-01-30 12:30:34 +0530

    add new post routes to verify user

--- routes/user.js
+++ routes/user.js
@@ -1,15 +1,27 @@
 const express = require("express");
 const router = express.Router();
-const authMiddleware = require("../middlewares/authMiddleware")
-const { handleCreateNewUser, handleLoginUser,handleRefreshUsers,handleLogoutUsers } = require("../controllers/user");
+const authMiddleware = require("../middlewares/authMiddleware");
+const {
+  handleCreateNewUser,
+  handleLoginUser,
+  handleRefreshUsers,
+  handleLogoutUsers,
+  handleVerifiedRequest,
+  handleVerifiedConfirm
+} = require("../controllers/user");
 const rateLimiter = require("../middlewares/rateLimiter");
+const verifiedMiddleware = require("../middlewares/verifiedMiddleware");
 
 router.post("/signup", handleCreateNewUser);
-router.post("/login",rateLimiter, handleLoginUser);
-router.post("/refresh",rateLimiter, handleRefreshUsers);
+router.post("/login", rateLimiter, handleLoginUser);
+
+router.post("/refresh", rateLimiter, handleRefreshUsers);
 router.post("/logout", handleLogoutUsers);
 
-router.get("/",authMiddleware,(req, res) => {
+router.post("/verify/request", authMiddleware, handleVerifiedRequest);
+router.post("/verify/confirm", authMiddleware, handleVerifiedConfirm);
+
+router.get("/", authMiddleware, verifiedMiddleware, (req, res) => {
   res.send("ok");
 });
 
