b97e8e01d8663a0fe6fdffc87f37732fd1f35bdb
Mohammed-ameen-co <aameenrangrej4@gmail.com>
2026-02-23 09:20:01 +0530

    add new routes

--- routes/user.js
+++ routes/user.js
@@ -10,6 +10,9 @@ const {
   handleLogoutUsers,
   handleVerifiedRequest,
   handleVerifiedConfirm,
+  handleForgetPassword,
+  handleResetPassword,
+  handleChangePassword,
   handleMe
 } = require("../controllers/user");
 const rateLimiter = require("../middlewares/rateLimiter");
@@ -19,7 +22,6 @@ router.post("/signup", handleEmailCreateNewUser);
 router.post("/login", rateLimiter, handleEmailLoginUser);
 
 router.post("/phone/request",handlePhoneUserOtpRequest);
-console.log("me phone verify route ke uper ")
 router.post("/phone/verify",handlePhoneUserOtpVerify);
 
 router.post("/refresh", rateLimiter, handleRefreshUsers);
@@ -27,6 +29,11 @@ router.post("/logout", authMiddleware ,handleLogoutUsers);
 
 router.post("/verify/request", authMiddleware, handleVerifiedRequest);
 router.post("/verify/confirm", authMiddleware, handleVerifiedConfirm);
+
+router.post("/forgot-password",handleForgetPassword);
+router.post("/reset-password",handleResetPassword);
+router.post("/change-password",handleChangePassword);
+
 
 router.get("/me",authMiddleware,handleMe)
 
